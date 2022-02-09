4584132661e81cc729256572663163b8630c8902
Svitlana <lanapavlovska90@gmail.com>
2022-02-09 21:34:37 +0200

    bootstrap changes

--- interactivity.js
+++ interactivity.js
@@ -2,3 +2,9 @@ let inputFocus = document.getElementById('search-input');
 inputFocus.addEventListener('mouseover', () => {
     inputFocus.focus();
 });
+
+let dropDown = document.getElementById('catalogue');
+dropDown.addEventListener('mouseover', () => {
+    dropDown.style.display = 'block';
+});
+console.log();
