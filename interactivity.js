98bc82ef3993aaee59310e322760631b733d1ab7
Svitlana <lanapavlovska90@gmail.com>
2022-02-19 14:40:12 +0200

    raw media querries for navigation and contacts

--- interactivity.js
+++ interactivity.js
@@ -3,8 +3,51 @@ inputFocus.addEventListener('mouseover', () => {
     inputFocus.focus();
 });
 
-let inputFocus1 = document.getElementById('search-input1');
-inputFocus1.addEventListener('mouseover', () => {
-    inputFocus1.focus();
+const navbar = document.querySelectorAll('.nav-sticky');
+window.addEventListener('scroll', () => {
+    navbar.forEach(function(bar) {
+bar.classList.toggle('sticky', window.scrollY>0);
+    })
+    
+    
+ 
+    
 });
+
+let galleryImages = document.querySelectorAll('.g-image');
+let getLatestOpenedImage;
+let windowWidth = window.innerWidth;
+if(galleryImages) {
+    galleryImages.forEach(function() {
+
+    })
+}
+
+function displaySearch () {
+    const searchInput = document.getElementById('search-input').value.toLowerCase();
+    const mainContent = document.getElementsByTagName('main');
+    let itemsAvailable = false;
+    const searchResult = [];
+    mainContent.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchInput)) {
+            let newDiv = document.createElement('div');
+            mainContent.appendChild(newDiv);
+            newDiv.appendChild(searchResult);
+            item.style.display = 'block';
+            itemsAvailable = true;
+            return searchResult.push(item);
+           
+        }
+
+        else {
+            item.display = 'none';
+        }
+    })
+
+}
+
+
+
+
+
 
